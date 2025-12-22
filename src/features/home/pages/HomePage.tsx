eae3403ec18aedb4f8c226ec73503c311f375a15
Alex Algarate <alex.algarate@gmail.com>
2025-12-22 21:23:25 +0100

    Home Page

---
+++ src/features/home/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import { Card } from '@shared/components/card/Card';
+import React from 'react';
+
+export const Component: React.FC = () => {
+  return (
+    <section>
+      <h1>Home Page</h1>
+
+      <Card>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, amet ipsam!
+          Tempore ut repellat aspernatur illum, amet doloremque nulla dolore id eaque
+          tempora minima eveniet consequuntur itaque facilis blanditiis quis?
+        </p>
+      </Card>
+      <Card>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, amet ipsam!
+          Tempore ut repellat aspernatur illum, amet doloremque nulla dolore id eaque
+          tempora minima eveniet consequuntur itaque facilis blanditiis quis?
+        </p>
+      </Card>
+      <Card>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, amet ipsam!
+          Tempore ut repellat aspernatur illum, amet doloremque nulla dolore id eaque
+          tempora minima eveniet consequuntur itaque facilis blanditiis quis?
+        </p>
+      </Card>
+      <Card>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, amet ipsam!
+          Tempore ut repellat aspernatur illum, amet doloremque nulla dolore id eaque
+          tempora minima eveniet consequuntur itaque facilis blanditiis quis?
+        </p>
+      </Card>
+    </section>
+  );
+};
