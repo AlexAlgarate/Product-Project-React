1699a46d98d04639c8be19dbeebb4a75cf075423
Alex Algarate <alex.algarate@gmail.com>
2025-12-25 22:12:33 +0100

    Refactor Card component adding style as optional prop

--- src/shared/components/card/Card.tsx
+++ src/shared/components/card/Card.tsx
@@ -5,11 +5,12 @@ import styles from './card.module.css'
 type Props = {
   readonly children: ReactNode;
   readonly title?: string;
+  readonly style?: React.CSSProperties;
 };
 
-export const Card: React.FC<Props> = ({ children, title }) => {
+export const Card: React.FC<Props> = ({ children, title, style }) => {
   return (
-    <div className={styles.card}>
+    <div style={style} className={styles.card}>
       {title && <h3>{title}</h3>}
       {children}
     </div>
