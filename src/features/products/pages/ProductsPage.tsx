ce028267bf0f54c831718b85445eb00f9c2042af
Alex Algarate <alex.algarate@gmail.com>
2025-12-25 22:29:39 +0100

    Basic render for /products

---
+++ src/features/products/pages/ProductsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const ProductsPage: React.FC = () => {
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <p>Página de productos</p>
+    </section>
+  );
+};
+
+export { ProductsPage as Component };
