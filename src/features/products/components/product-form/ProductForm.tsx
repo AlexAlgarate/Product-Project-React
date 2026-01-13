462d482b2ab09e478910e6f24d4f5e7cb91467eb
Alex Algarate <alex.algarate@gmail.com>
2026-01-13 19:52:18 +0100

    refactor LabelInput in productForm

--- src/features/products/components/product-form/ProductForm.tsx
+++ src/features/products/components/product-form/ProductForm.tsx
@@ -13,11 +13,56 @@ const newProduct: Product = {
   name: '',
   price: 0,
   tags: [],
-  isOnSale: true,
+  isOnSale: false,
   image: '',
   description: '',
 } as Product;
 
+type LabelProps = {
+  htmlFor: string;
+  spanText: string;
+  idInput: string;
+  nameInput: string;
+  valueInput?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  as?: 'input' | 'textarea';
+};
+
+const LabelInput: React.FC<LabelProps> = ({
+  htmlFor,
+  spanText,
+  idInput,
+  nameInput,
+  valueInput = '',
+  onChange,
+  as = 'input',
+}) => {
+  return (
+    <label className="flex gap-4" htmlFor={htmlFor}>
+      <span>{spanText}</span>
+
+      {as === 'textarea' ? (
+        <textarea
+          className="border rounded-md "
+          id={idInput}
+          name={nameInput}
+          value={valueInput ?? ''}
+          onChange={onChange}
+          rows={4}
+        />
+      ) : (
+        <input
+          className="border rounded-md"
+          type="text"
+          id={idInput}
+          name={nameInput}
+          value={valueInput}
+          onChange={onChange}
+        />
+      )}
+    </label>
+  );
+};
 export const ProductForm: React.FC<ProductFormProps> = ({ item, onClose }) => {
   const isEditing = Boolean(item);
 
@@ -31,7 +76,9 @@ export const ProductForm: React.FC<ProductFormProps> = ({ item, onClose }) => {
     onClose(null);
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
     const { value, name } = event.target;
     setProduct({
       ...product,
@@ -40,36 +87,57 @@ export const ProductForm: React.FC<ProductFormProps> = ({ item, onClose }) => {
   };
 
   return (
-    <Card title={product.name || 'Añadir vehículo'}>
-      <form className="product-form" onSubmit={handleSave} onReset={handleReset}>
-        {isEditing && 'Campo de solo lectura'}
-        <div
-          style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
-          className="control-group"
-        >
-          <label htmlFor="name">
-            <span>name:</span>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={product.name}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="price">
-            <span>Price:</span>
-            <input
-              type="text"
-              id="model"
-              name="price"
-              value={product.price}
-              onChange={handleChange}
-            />
-          </label>
+    <Card
+      style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
+      title={product.name || 'Producto'}
+    >
+      <form className="flex flex-col gap-6" onSubmit={handleSave} onReset={handleReset}>
+        {isEditing}
+        <div className="flex flex-col gap-6">
+          <LabelInput
+            htmlFor="name"
+            spanText="Name"
+            idInput="name"
+            nameInput="name"
+            valueInput={product.name}
+            onChange={handleChange}
+          />
+          <LabelInput
+            htmlFor="price"
+            spanText="Price:"
+            idInput="price"
+            nameInput="price"
+            valueInput={String(product.price)}
+            onChange={handleChange}
+          />
+          <LabelInput
+            htmlFor="tags"
+            spanText="Tags:"
+            idInput="tags"
+            nameInput="tags"
+            valueInput={product.tags.join(', ')}
+            onChange={handleChange}
+          />
+          <LabelInput
+            htmlFor="image"
+            spanText="Image:"
+            idInput="image"
+            nameInput="image"
+            valueInput={product.image}
+            onChange={handleChange}
+          />
+          <LabelInput
+            htmlFor="description"
+            spanText="Description:"
+            idInput="description"
+            nameInput="description"
+            valueInput={product.description}
+            onChange={handleChange}
+            as="textarea"
+          />
         </div>
 
-        <div className="buttons-group">
+        <div>
           <button type="submit">{isEditing ? 'Guardar' : 'Añadir'}</button>
           <button type="reset">Cancelar</button>
         </div>
