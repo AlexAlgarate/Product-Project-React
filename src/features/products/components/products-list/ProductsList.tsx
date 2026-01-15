28b1ba5bc19075380fc9f39ea913c1b339bb3ac2
Alex Algarate <alex.algarate@gmail.com>
2026-01-15 23:05:12 +0100

    refactor not product available component

--- src/features/products/components/products-list/ProductsList.tsx
+++ src/features/products/components/products-list/ProductsList.tsx
@@ -7,6 +7,33 @@ import { useProducts } from './useProducts';
 import { Button } from '@shared/components/ui';
 import { Routes } from '@shared/utils/constants';
 
+const NotProductsAvailable: React.FC = () => {
+  return (
+    <Card
+      className="
+      relative flex flex-col items-center gap-6
+      rounded-2xl border border-white/10 bg-linear-to-b
+    from-white/5 to-transparent p-10 text-center
+      shadow-lg backdrop-blur"
+    >
+      <h2 className="text-2xl font-semibold tracking-tight text-orange-600/80">
+        No hay productos disponibles
+      </h2>
+
+      <p className="max-w-md text-white/80">
+        Aún no has creado ningún producto. Empieza ahora y añade el primero a tu
+        catálogo.
+      </p>
+
+      <a href={Routes.newProduct}>
+        <Button variant="primary" size="lg">
+          Crear mi primer producto
+        </Button>
+      </a>
+    </Card>
+  );
+};
+
 export const ProductsList: React.FC = () => {
   const [showForm, setShowForm] = useState<boolean>(false);
   const [activeProduct, setActiveProduct] = useState<Product | null>(null);
@@ -47,14 +74,7 @@ export const ProductsList: React.FC = () => {
   }
 
   if (products.length === 0) {
-    return (
-      <div className="flex flex-col">
-        NO HAY PRODUCTOS DISPONIBLES
-        <a href={Routes.newProduct}>
-          <Button variant="danger">Crea tu primer producto</Button>
-        </a>
-      </div>
-    );
+    return <NotProductsAvailable />;
   }
 
   return (
