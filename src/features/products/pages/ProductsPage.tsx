87f510961dcfc23155b53c9d229c4b37cd1060be
Alex Algarate <alex.algarate@gmail.com>
2026-01-16 22:45:27 +0100

    rename Products List

--- src/features/products/pages/ProductsPage.tsx
+++ src/features/products/pages/ProductsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router';
-import { ProductsList } from '../components/products-list/ProductsList';
+import { ProductsList } from '../components/ProductsList/ProductsList';
 import { ProductDetail } from '../components/ProductDetail/ProductDetail';
 
 export const ProductsPage: React.FC = () => {
